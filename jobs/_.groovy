1e1a5365c24a3ac1941254a96c890025fbec6347
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-11-18 10:19:56 +0000

    Typo in DSL

--- jobs/_.groovy
+++ jobs/_.groovy
@@ -2,4 +2,4 @@ new eap7.Builder(branch:'7.4.x').build(this)
 
 new eap7.Builder(branch:'7.4.x-proposed').build(this)
 
-EAPView.jobList(this, 'eap-7.4.x', /eap-7\.4\/.*/)
+EapView.jobList(this, 'eap-7.4.x', /eap-7\.4\/.*/)
