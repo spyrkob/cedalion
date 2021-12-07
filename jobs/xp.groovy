45680f62bee332b523d72895d458a9f4aa16fc37
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-12-07 14:23:12 +0000

    add xp jobs

---
+++ jobs/xp.groovy
@@ -0,0 +1,12 @@
+new eap7.Builder(branch:'xp2',
+                 jobName: 'xp-2.0.x',
+                ).buildAndTest(this)
+EapView.jobList(this, 'xp-2.0.x', 'xp-2.0.x.*')
+
+new eap7.Builder(branch:'xp-3.0.x',
+                 jobName: 'xp-3.0.x',
+                ).buildAndTest(this)
+new eap7.Builder(branch:'xp-3.0.x-proposed',
+                 jobName: 'xp-3.0.x-proposed',
+                ).buildAndTest(this)
+EapView.jobList(this, 'xp-3.0.x', 'xp-3.0.x.*')
