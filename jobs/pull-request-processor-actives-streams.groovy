683e05c8d93f922cfc924d8c5994490b040f0ff5
Bartosz Spyrko-Smietanko <bspyrkos@redhat.com>
2021-12-07 13:43:24 +0000

    PR processor job

---
+++ jobs/pull-request-processor-actives-streams.groovy
@@ -0,0 +1,50 @@
+pipelineJob(ITEM_NAME) {
+
+    definition {
+        cps {
+        script(readFileFromWorkspace('pipelines/pr-processor-pipeline'))
+        sandbox()
+        }
+    }
+    properties {
+        disableConcurrentBuilds {
+            abortPrevious(false)
+        }
+    }
+    logRotator {
+        daysToKeep(30)
+        numToKeep(10)
+        artifactDaysToKeep(60)
+        artifactNumToKeep(5)
+    }
+    triggers {
+        scm ('H/10 * * * *')
+        cron('@daily')
+    }
+    parameters {
+        stringParam {
+            name ("PULL_REQUEST_PROCESSOR_HOME")
+            defaultValue('/opt/tools/pr-processor/')
+        }
+        stringParam {
+            name ("HARMONIA_SCRIPT")
+            defaultValue('pr-processor.sh')
+        }
+        stringParam {
+            name ("JAVA_HOME")
+            defaultValue("/opt/oracle/java")
+        }
+        stringParam {
+            name ("MAVEN_HOME")
+            defaultValue("/opt/oracle/maven")
+        }
+        stringParam {
+            name ("MAVEN_SETTINGS_XML")
+            defaultValue('/opt/tools/settings.xml')
+        }
+        stringParam {
+            name ("VERSION")
+            defaultValue("0.8.10.Final")
+        }
+    }
+}
