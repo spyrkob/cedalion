f49078d9c1c9eceb4910bd7a6f19d0174b0306e7
Romain Pelisse <belaran@gmail.com>
2022-01-10 17:28:45 +0100

    ansible-ci: add supports for running molecule tests

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -1,4 +1,10 @@
+// Jobs to release productized version of Ansible Collection
 new ansible.Builder(collectionName:'redhat-csp-download').build(this)
 new ansible.Builder(collectionName:'jws-ansible-playbook').build(this)
 new ansible.Builder(collectionName:'ansible_collections_jcliff').build(this)
+// CI Jobs for Ansible Middleware
+//   Note that each CI job needs to increment the moleculeBuildId as
+//   this translate into a port number for SSHd running on the slave
+//   container (and thus, needs to be unique).
+new ansibleCi.Builder(projectName:'jws-ansible-playbook', moleculeBuildId: 22001).build(this)
 EapView.jobList(this, 'Ansible Collections', 'ansible-collection-*')
