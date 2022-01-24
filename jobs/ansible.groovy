686cfc27183d7382b3acd2b040dc588b2b5c4952
Romain Pelisse <belaran@gmail.com>
2022-01-24 19:43:57 +0100

    Add infinispan & jcliff jobs to Ansible CI

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -2,11 +2,13 @@
 new ansible.Builder(collectionName:'redhat-csp-download').build(this)
 new ansible.Builder(collectionName:'jws-ansible-playbook').build(this)
 new ansible.Builder(collectionName:'ansible_collections_jcliff').build(this)
+EapView.jobList(this, 'Ansible Collections', 'ansible-collection.*')
 // CI Jobs for Ansible Middleware
 //   Note that each CI job needs to increment the moleculeBuildId as
 //   this translate into a port number for SSHd running on the slave
 //   container (and thus, needs to be unique).
 new ansibleCi.Builder(projectName:'jws-ansible-playbook', moleculeBuildId: 22001).build(this)
 new ansibleCi.Builder(projectName:'wildfly', moleculeBuildId: 23001).build(this)
-EapView.jobList(this, 'Ansible Collections', 'ansible-collection.*')
+new ansibleCi.Builder(projectName:'ansible_collections_jcliff', moleculeBuildId: 24001).build(this)
+new ansibleCi.Builder(projectName:'infinispan', moleculeBuildId: 25001).build(this)
 EapView.jobList(this, 'Ansible CI', 'ansible-ci.*')
