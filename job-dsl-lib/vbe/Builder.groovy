349ff01ff75cdd9814a9b9086d719e4c6e59b069
baranowb <baranowb@gmail.com>
2022-03-16 15:38:49 +0100

    Introduce VBE parts

---
+++ job-dsl-lib/vbe/Builder.groovy
@@ -0,0 +1,100 @@
+package vbe
+
+import util.JobSharedUtils
+
+class Builder {
+
+    String jobName
+    String branch
+    String schedule = JobSharedUtils.DEFAULT_SCHEDULE
+    String parentJobname = ''
+    String mavenSettingsXml
+    String javaHome='/opt/oracle/java'
+    String harmoniaScript = 'eap-job/olympus.sh'
+    String gitRepositoryUrl = 'git@github.com:jbossas/jboss-eap7.git'
+
+    def buildAndTest(factory) {
+        build(factory)
+        test(factory)
+    }
+
+    def build(factory) {
+
+        if (jobName == null) {
+            jobName = 'vbe-eap-' + branch
+        }
+
+        factory.with {
+            pipelineJob(jobName + '-build') {
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('pipelines/vbe-wildfly-pipeline'))
+                        sandbox()
+                    }
+                }
+                JobSharedUtils.defaultBuildDiscarder(delegate)
+                triggers {
+                    scm(schedule)
+                }
+                parameters {
+                    commonParameters(delegate)
+                    stringParam {
+                        name ("VBE_EXTENSION")
+                        defaultValue('Anything')
+                        description('If present, VBE will run, dont change this.')
+                    }
+                    stringParam {
+                        name ("VBE_REPOSITORY_NAMES")
+                        defaultValue('jboss-eap-7.4-product-repository')
+                        description('Comma separated list of repository names - as present in maven, if present, this will be used to trim repositories present in reactor.')
+                    }
+                    stringParam {
+                        name ("VBE_CHANNELS")
+                        defaultValue('https://gitlab.cee.redhat.com/bspyrkos/channels-definitions/-/raw/main/eap-74/src/main/resources/channel.yaml?inline=false')
+                        description('Comma seprated list of URIs to yaml channels')
+                    }
+                    stringParam {
+                        name ("VBE_LOG_FILE")
+                        defaultValue('../vbe.update.log.yaml')
+                        description('Log file name, if not set, default will be used')
+                    }
+                }
+            }
+        }
+    }
+
+    def test(factory) {
+        factory.with {
+            pipelineJob(jobName + '-testsuite') {
+
+                definition {
+                    cps {
+                        script(readFileFromWorkspace('pipelines/vbe-wildfly-pipeline'))
+                        sandbox()
+                    }
+                }
+                JobSharedUtils.defaultBuildDiscarder(delegate)
+                parameters {
+                    commonParameters(delegate)
+                    stringParam {
+                        name("PARENT_JOBNAME")
+                        defaultValue(parentJobname)
+                    }
+                }
+            }
+        }
+    }
+
+    def commonParameters(params) {
+        JobSharedUtils.gitParameters(params, gitRepositoryUrl, branch)
+        JobSharedUtils.mavenParameters(params: params, javaHome: javaHome, mavenSettingsXml: mavenSettingsXml)
+        params.with {
+            stringParam {
+                name ("HARMONIA_SCRIPT")
+                defaultValue(harmoniaScript)
+            }
+        }
+    }
+}
+
--- jobs/ci_jobs.groovy
+++ jobs/ci_jobs.groovy
@@ -14,6 +14,6 @@ new ci_jobs.Builder(repoName: 'harmonia', branch: 'olympus').buildBashJob(this)
 
 new ci_jobs.Builder(jobName: 'ci-harmonia-cci', repoName: 'harmonia', branch: 'olympus-cci').buildBashJob(this)
 
-new ci_jobs.Builder(repoName: 'maven-vbe').buildMvnJob(this)
+new ci_jobs.Builder(repoName: 'maven-vbe', javaHome: "/opt/oracle/openjdk-11.0.14.1_1").buildMvnJob(this)
 
 EapView.jobList(this, 'SET CI', 'ci.*')
