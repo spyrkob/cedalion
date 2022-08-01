572d32a429dfdb2ba217b6e9c01da637782a541f
Bartosz Spyrko-Smietanko <spyrkob@gmail.com>
2022-08-01 11:16:19 +0100

    Add prospero build

---
+++ jobs/prospero.groovy
@@ -0,0 +1,26 @@
+import util.JobSharedUtils
+
+pipelineJob(jobName) {
+    // for some reason readFileFromWorkspace doesn't work from within baseJob call
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelines/mvn-pipeline'))
+            sandbox()
+        }
+    }
+    parameters {
+        JobSharedUtils.gitParameters(delegate, 'https://github.com/wildfly-extras/prospero.git', 'master')
+        JobSharedUtils.mavenParameters(params: delegate, javaHome: javaHome)
+        // override MAVEN_OPTS to add -Dnorpm
+        stringParam {
+            name ("MAVEN_OPTS")
+            defaultValue("-Dmaven.wagon.http.ssl.insecure=true -Dhttps.protocols=TLSv1.2  -Dnorpm")
+        }
+        stringParam {
+            name ("MAVEN_GOALS")
+            defaultValue(mavenGoals)
+        }
+    }
+}
+
+EapView.jobList(this, 'prospero')
