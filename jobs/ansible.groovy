baec25d5db9c95635ad6b13fc35f257967fb3185
Guido Grazioli <ggraziol@redhat.com>
2022-08-26 15:39:13 +0200

    Rename jws-ansible-playbook to jws, add downstr job for amq

--- jobs/ansible.groovy
+++ jobs/ansible.groovy
@@ -1,6 +1,6 @@
 // Jobs to release productized version of Ansible Collection
 new ansibleCollection.Builder(collectionName:'redhat-csp-download').build(this)
-new ansibleCollection.Builder(collectionName:'jws-ansible-playbook').build(this)
+new ansibleCollection.Builder(collectionName:'jws').build(this)
 new ansibleCollection.Builder(collectionName:'ansible_collections_jcliff').build(this)
 new ansibleCollection.Builder(collectionName:'wildfly', downstreamName: 'jboss_eap').build(this)
 new ansibleCollection.Builder(collectionName:'infinispan', downstreamName: 'jboss_data_grid').build(this)
@@ -11,7 +11,7 @@ EapView.jobList(this, 'Ansible Collections', 'ansible-collection.*')
 //   Note that each CI job needs to increment the moleculeBuildId as
 //   this translate into a port number for SSHd running on the slave
 //   container (and thus, needs to be unique).
-new ansibleCi.Builder(projectName:'jws-ansible-playbook', moleculeBuildId: 22001).build(this)
+new ansibleCi.Builder(projectName:'jws', moleculeBuildId: 22001).build(this)
 new ansibleCi.Builder(projectName:'wildfly', moleculeBuildId: 23001).build(this)
 new ansibleCi.Builder(projectName:'infinispan', moleculeBuildId: 25001).build(this)
 new ansibleCi.Builder(projectName:'keycloak', moleculeBuildId: 26001).build(this)
@@ -26,7 +26,7 @@ new ansibleCi.Builder(projectName:'jws-app-update-demo', branch: 'main', project
 EapView.jobList(this, 'Ansible Demos', '^.*-demo')
 // Janus jobs - generating downstream collections
 new ansible.Builder(projectName:'janus', jobSuffix: '-redhat_csp_download', playbook: 'playbooks/redhat_csp_download.yml').build(this)
-new ansible.Builder(projectName:'janus', jobSuffix: '-jws-ansible-playbook', playbook: 'playbooks/jws.yml').build(this)
+new ansible.Builder(projectName:'janus', jobSuffix: '-jws', playbook: 'playbooks/jws.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-jboss_eap', playbook: 'playbooks/jboss_eap.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-jboss_data_grid', playbook: 'playbooks/jboss_data_grid.yml').build(this)
 new ansible.Builder(projectName:'janus', jobSuffix: '-rh_sso', playbook: 'playbooks/rh_sso.yml').build(this)
@@ -46,6 +46,7 @@ new ansibleDownstreamRunner.Builder(
   products_paths: '/eap7/7.4.5/jboss-eap-7.4.5.zip'
   ).build(this)
 EapView.jobList(this, 'Ansible Downstream Runner', '^ansible-downstream-runner-.*$')
-new ansibleDownstreamCi.Builder(projectName: 'jws-ansible-playbook', moleculeBuildId: 50001).build(this)
+new ansibleDownstreamCi.Builder(projectName: 'jws', moleculeBuildId: 50001).build(this)
 new ansibleDownstreamCi.Builder(projectName: 'jboss_eap', moleculeBuildId: 50002).build(this)
+new ansibleDownstreamCi.Builder(projectName: 'amq', moleculeBuildId: 50003).build(this)
 EapView.jobList(this, 'Ansible Downstream CI', 'ansible-downstream-ci.*$')
